345874657784df84e871be863a9d80f55623addc
Joschar22 <chaqquererj@gmail.com>
2023-09-20 21:51:11 -0500

    Finalizando Insertar(creat), y fin de CRUD

--- index.php
+++ index.php
@@ -11,59 +11,78 @@
   <?php
   include("conexion.php");
 
-  $registros=$base->query("select * from datos_usuarios")->fetchAll(PDO::FETCH_OBJ);
+  $registros = $base->query("select * from datos_usuarios")->fetchAll(PDO::FETCH_OBJ);
 
   //$conexion=$base->query("select * from datos_usuarios");
-
-  //$registros=$conexion->fetchAll(PDO::FETCH_OBJ);
-
-
-
-
-
   
+  //$registros=$conexion->fetchAll(PDO::FETCH_OBJ);
+  
+if (isset($_POST["cr"])) {
+  $nombre=$_POST["Nom"];
+  $apellido=$_POST["Ape"];
+  $direccion=$_POST["Dir"];
+
+  $sql="insert into datos_usuarios (NOMBRE, APELLIDO, DIRECCION) VALUES (:nom, :ape, :dir)";
+  $resultado=$base->prepare(($sql));
+  $resultado->execute(array(":nom"=>$nombre, ":ape"=>$apellido, ":dir"=>$direccion));
+
+  header("Location:index.php");
+  
+}
   ?>
 
   <h1>CRUD<span class="subtitulo">Create Read Update Delete</span></h1>
 
-  <table width="50%" border="0" align="center">
-    <tr>
-      <td class="primera_fila">Id</td>
-      <td class="primera_fila">Nombre</td>
-      <td class="primera_fila">Apellido</td>
-      <td class="primera_fila">Dirección</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-    </tr>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <table width="50%" border="0" align="center">
+      <tr>
+        <td class="primera_fila">Id</td>
+        <td class="primera_fila">Nombre</td>
+        <td class="primera_fila">Apellido</td>
+        <td class="primera_fila">Dirección</td>
+        <td class="sin">&nbsp;</td>
+        <td class="sin">&nbsp;</td>
+        <td class="sin">&nbsp;</td>
+      </tr>
 
-<?php
-foreach ($registros as $persona):
-?>
-    <tr>
-      <td><?php echo $persona->Id?></td>
-      <td><?php echo $persona->Nombre?></td>
-      <td><?php echo $persona->Apellido?></td>
-      <td><?php echo $persona->Direccion?></td>
+      <?php
+      foreach ($registros as $persona):
+        ?>
+        <tr>
+          <td>
+            <?php echo $persona->Id ?>
+          </td>
+          <td>
+            <?php echo $persona->Nombre ?>
+          </td>
+          <td>
+            <?php echo $persona->Apellido ?>
+          </td>
+          <td>
+            <?php echo $persona->Direccion ?>
+          </td>
 
-      <td class="bot" ><a href="borrar.php?id=<?php echo $persona-> Id ?>"><input type='button' name='del' id='del' value='Borrar'></a></td>
-      <td class='bot'><a href="editar.php?id=<?php echo $persona-> Id ?> & 
-      nom=<?php echo $persona-> Nombre ?>& 
-      ape=<?php echo $persona-> Apellido ?>& 
-      dir=<?php echo $persona-> Direccion ?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
-    </tr>
-<?php
-endforeach ;
-?>
+          <td class="bot"><a href="borrar.php?id=<?php echo $persona->Id ?>"><input type='button' name='del' id='del'
+                value='Borrar'></a></td>
+          <td class='bot'><a href="editar.php?id=<?php echo $persona->Id ?> & 
+      nom=<?php echo $persona->Nombre ?>& 
+      ape=<?php echo $persona->Apellido ?>& 
+      dir=<?php echo $persona->Direccion ?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
+        </tr>
+        <?php
+      endforeach;
+      ?>
 
-    <tr>
-      <td></td>
-      <td><input type='text' name='Nom' size='10' class='centrado'></td>
-      <td><input type='text' name='Ape' size='10' class='centrado'></td>
-      <td><input type='text' name=' Dir' size='10' class='centrado'></td>
-      <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td>
-    </tr>
-  </table>
+      <tr>
+        <td></td>
+        <td><input type='text' name='Nom' size='10' class='centrado'></td>
+        <td><input type='text' name='Ape' size='10' class='centrado'></td>
+        <td><input type='text' name=' Dir' size='10' class='centrado'></td>
+        <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td>
+      </tr>
+    </table>
+
+  </form>
 
 
   <p>&nbsp;</p>
